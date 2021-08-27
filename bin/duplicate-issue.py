e72049e45e9b4690dba9a03e9c218caabd3ef37f
Paul Downey <psd@whatfettle.com>
2021-08-27 09:28:43 +0100

    Find all issues

--- bin/duplicate-issue.py
+++ bin/duplicate-issue.py
@@ -9,6 +9,5 @@ for path in glob.glob('var/issue/*/*.csv'):
     for row in csv.DictReader(open(path, newline="")):
         key = row["row-number"] + "," + row["field"]
         if key in seen:
-            if key == "2,LastUpdatedDate" and seen[key]["value"] == "2018-21-21" and row["value"] == "2017-12-21":
-                print(path, row)
+            print(path, row)
         seen[key] = row
