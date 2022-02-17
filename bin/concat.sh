daa5b572fb719b98b4628dd9c76cbec1b6085ebe
Paul Downey <psd@whatfettle.com>
2022-02-17 14:59:08 +0000

    Migrate dataset, fix Nones

--- bin/concat.sh
+++ bin/concat.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
+set -e
+
 python3 bin/concat-source.py | python3 bin/fixdates.py dataset/source.csv
 for file in endpoint.csv log.csv resource.csv
 do
+    set -x
     python3 bin/csvcat.py var/collection/*/$file | python3 bin/fixdates.py dataset/$file
+    set +x
 done
 
 for file in column.csv concat.csv convert.csv default.csv patch.csv skip.csv transform.csv filter.csv lookup.csv
--- bin/csvcat.py
+++ bin/csvcat.py
@@ -13,7 +13,12 @@ for path in paths:
         reader = csv.reader(f)
         headers = next(reader)
         for h in headers:
-            if h not in fieldnames:
+
+            # migrate pipeline to dataset
+            if h == "pipeline":
+                h = "dataset"
+
+            if h and h not in fieldnames:
                 fieldnames.append(h)
 
 writer = csv.DictWriter(sys.stdout, fieldnames=fieldnames)
@@ -22,4 +27,9 @@ for path in paths:
     with open(path, "r", newline="") as f:
         reader = csv.DictReader(f)
         for line in reader:
+
+            # migrate pipeline to dataset
+            if "dataset" in fieldnames and not line.get("dataset", ""):
+                line["dataset"] = line.get("pipeline", "")
+
             writer.writerow(line)
