e3019244eeacb3c3569d33c262179d2e8556dc99
Paul Downey <psd@whatfettle.com>
2022-03-09 18:19:52 +0000

    Make entry-date a timestamp, other dates YYYY-MM-DD

--- bin/fixdates.py
+++ bin/fixdates.py
@@ -2,11 +2,49 @@
 
 import sys
 import csv
+from datetime import datetime
 
 w = None
 fields = None
 
 print(sys.argv[1])
+
+
+def as_timestamp(date):
+    if not date:
+        return ""
+    if len(date) >= 20:
+        dt = datetime.strptime(date[:19], "%Y-%m-%dT%H:%M:%S")
+    elif len(date) == 20:
+        dt = datetime.strptime(date, "%Y-%m-%dT%H:%M:%SZ")
+    elif len(date) == 10:
+        dt = datetime.strptime(date, "%Y-%m-%d")
+    elif len(date) == 7:
+        dt = datetime.strptime(date, "%Y-%m")
+    elif len(date) == 4:
+        dt = datetime.strptime(date, "%Y")
+    else:
+        print("unknown date format", date)
+        sys.exit(2)
+
+    return dt.strftime("%Y-%m-%dT%H:%M:%SZ")
+
+
+def as_date(date):
+    if not date:
+        return ""
+    if len(date) == 10:
+        dt = datetime.strptime(date, "%Y-%m-%d")
+    elif len(date) == 7:
+        dt = datetime.strptime(date, "%Y-%m")
+    elif len(date) == 4:
+        dt = datetime.strptime(date, "%Y")
+    else:
+        print("unknown date format", date)
+        sys.exit(2)
+
+    return dt.strftime("%Y-%m-%d")
+
 
 for row in csv.DictReader(sys.stdin):
     if not w:
@@ -15,14 +53,9 @@ for row in csv.DictReader(sys.stdin):
         w.writeheader()
 
     for col in row:
-        if col.endswith("-date"):
-            s = row[col]
-            if len(s) > 10:
-                s = s[:10]
-            elif len(s) == 7:
-                s = s + "-01"
-            elif len(s) == 4:
-                s = s + "-01-01"
-            row[col] = s
+        if col == "entry-date":
+            row[col] = as_timestamp(row[col])
+        elif col.endswith("-date"):
+            row[col] = as_date(row[col])
 
     w.writerow(row)
