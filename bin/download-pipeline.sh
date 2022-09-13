0897b2068089ba4e4427da01986ea5d5100e5b1a
Paul Downey <psd@whatfettle.com>
2022-09-13 17:48:42 +0100

    Download combine.csv

--- bin/download-pipeline.sh
+++ bin/download-pipeline.sh
@@ -5,7 +5,7 @@ github="https://raw.githubusercontent.com/digital-land/"
 csvcut -c collection specification/collection.csv | tail -n +2 |
 while read collection
 do
-    for file in column.csv concat.csv convert.csv default.csv default-value.csv patch.csv plugins.py skip.csv transform.csv filter.csv lookup.csv
+    for file in column.csv combine.csv concat.csv convert.csv default.csv default-value.csv patch.csv plugins.py skip.csv transform.csv filter.csv lookup.csv
     do
         dir=var/pipeline/$collection
         path=var/pipeline/$collection/$file
