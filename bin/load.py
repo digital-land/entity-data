fd55d16ff006517fc81c231d1760949270a68e48
Chris Johns <95475146+cjohns-scottlogic@users.noreply.github.com>
2024-04-17 08:47:04 +0100

    Updated expectations table name. (#13)

--- bin/load.py
+++ bin/load.py
@@ -59,7 +59,7 @@ tables = {
 
     "issue": "issues",
 
-    "expectation-response": "expectations",
+    "expectation-result": "expectations",
     "expectation-issue": "expectations",
 }
 
@@ -73,7 +73,7 @@ indexes = {
     "resource_organisation": ["resource", "organisation"],
     "specification_dataset": ["specification", "dataset"],
     "provision": ["organisation", "dataset", "project", "cohort", "provision_reason"],
-    "expectation-issue": ["response_id"],
+    "expectation-issue": ["expectation-result"],
 }
 
 
