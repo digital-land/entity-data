3e88d472ab088d61623543b67c2f60a1c0873c8e
Samriti Sadhu <ssadhu@scottlogic.com>
2024-10-17 15:38:21 +0100

    Adding old-resource filter

--- bin/resources.py
+++ bin/resources.py
@@ -27,22 +27,36 @@ for row in csv.DictReader(open("collection/source.csv", newline="")):
                 endpoints[endpoint]["pipelines"][pipeline] = True
                 endpoints[endpoint]["collection"] = row["collection"]
 
+old_resources_map = {}
+
+# Load old-resource.csv to create a mapping of old to updated resources
+for row in csv.DictReader(open("collection/old-resource.csv", newline="")):
+    old_resource = row["old-resource"]
+    updated_resource = row["resource"]
+    old_resources_map[old_resource] = updated_resource
+
 # load resources
 for row in csv.DictReader(open("collection/resource.csv", newline="")):
     resource = row["resource"]
+
+    # Skip this resource if it's an old resource
+    if resource in old_resources_map:
+        continue
     resources[resource] = row
     resources[resource].setdefault("pipelines", {})
     resources[resource]["endpoints"] = row["endpoints"].split(";")
 
     # add collections to the resource
     for endpoint in resources[resource]["endpoints"]:
-        endpoints[endpoint].setdefault("resource", {})
-        endpoints[endpoint][resource] = True
-        resources[resource]["collection"] = endpoints[endpoint]["collection"]
+        if endpoint in endpoints:
+            endpoints[endpoint].setdefault("resource", {})
+            endpoints[endpoint][resource] = True
+            resources[resource]["collection"] = endpoints[endpoint]["collection"]
 
-        # add pipelines to resource
-        for pipeline in endpoints[endpoint]["pipelines"]:
-            resources[resource]["pipelines"][pipeline] = True
+            # add pipelines to resource
+            for pipeline in endpoints[endpoint]["pipelines"]:
+                resources[resource]["pipelines"][pipeline] = True
+
 
 # https://digital-land-production-collection-dataset.s3.eu-west-2.amazonaws.com/{COLLECTION}-collection/issue/{PIPELINE}/{RESOURCE}.csv
 for resource in resources:
