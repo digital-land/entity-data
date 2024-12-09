5564771167a9a31a482cf9dd385c186a753bdae7
eveleighoj <35256612+eveleighoj@users.noreply.github.com>
2024-12-09 15:39:54 +0000

    add file downloader and versions

--- bin/download_expectations.py
+++ bin/download_expectations.py
@@ -11,28 +11,31 @@ import duckdb
 
 from pathlib import Path
 from urllib.error import HTTPError
+from  datetime import datetime
 
 from urllib.request import urlretrieve
 import pandas as pd
 from digital_land.specification import Specification
 
+from file_downloader import download_urls
+
 spec = Specification('specification')
 cache_exp_dir = 'var/expectations'
 Path(cache_exp_dir).mkdir(parents=True, exist_ok=True)
+now = datetime.now()
+timestamp = int(now.replace(minute=0, second=0, microsecond=0).timestamp())
 
 datasets = spec.dataset
+url_map =  {}
 for dataset_obj in datasets.values():
     if dataset_obj['collection']:
         s3="https://files.planning.data.gov.uk"
         dataset = dataset_obj['dataset']
-        print(dataset)
-        # https://files.planning.data.gov.uk/log/expectations/dataset=ancient-woodland/ancient-woodland.parquet
-        try:
-            url = f"https://files.planning.data.gov.uk/log/expectation/dataset={dataset}/{dataset}.parquet"
-            output_file = cache_exp_dir + f"/{dataset}.parquet"
-            urlretrieve(url, output_file)
-        except HTTPError as e:
-            logging.error(f"unable to download file because {e}")
+        url = f"https://files.planning.data.gov.uk/log/expectation/dataset={dataset}/{dataset}.parquet?version={timestamp}"
+        output_path = cache_exp_dir + f"/{dataset}.parquet"
+        url_map[url] = output_path
+
+download_urls(url_map)
 
 conn = duckdb.connect()
 Path('expectation').mkdir(parents=True, exist_ok=True)
