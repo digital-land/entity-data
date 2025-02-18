b2c92d55851d00392dc9640645fc167cae0a8511
CarlosCoelhoSL <ccoelho@scottlogic.com>
2025-02-18 16:52:19 +0000

    adds check_performance

---
+++ check/check_performance.py
@@ -0,0 +1,32 @@
+import sqlite3
+from digital_land.expectations.operation import (
+    check_columns
+)
+
+EXPECTED = {
+    "endpoint_dataset_issue_type_summary": ['organisation', 'organisation_name', 'cohort', 'dataset', 'collection', 'pipeline', 'endpoint', 'endpoint_url', 'resource', 'resource_start_date', 'resource_end_date', 'latest_log_entry_date', 'count_issues', 'date', 'issue_type', 'severity', 'responsibility', 'fields'],
+    "endpoint_dataset_resource_summary": ['organisation', 'organisation_name', 'cohort', 'dataset', 'collection', 'pipeline', 'endpoint', 'endpoint_url', 'resource', 'resource_start_date', 'resource_end_date', 'latest_log_entry_date', 'mapping_field', 'non_mapping_field'],
+    "endpoint_dataset_summary": ['organisation', 'dataset', 'endpoint', 'endpoint_url', 'resource', 'latest_status', 'latest_exception', 'latest_log_entry_date', 'entry_date', 'end_date', 'latest_resource_start_date', 'resource_end_date'],
+    "provision_summary": ['organisation', 'organisation_name', 'dataset', 'provision_reason', 'active_endpoint_count', 'error_endpoint_count', 'count_issue_error_internal', 'count_issue_error_external', 'count_issue_warning_internal', 'count_issue_warning_external', 'count_issue_notice_internal', 'count_issue_notice_external'],
+    "reporting_historic_endpoints": ['organisation', 'name', 'organisation_name', 'dataset', 'collection', 'pipeline', 'endpoint', 'endpoint_url', 'licence', 'latest_status', 'latest_exception', 'resource', 'latest_log_entry_date', 'endpoint_entry_date', 'endpoint_end_date', 'resource_start_date', 'resource_end_date'],
+    "reporting_latest_endpoints": ['organisation', 'name', 'organisation_name', 'dataset', 'collection', 'pipeline', 'endpoint', 'endpoint_url', 'licence', 'latest_status', 'days_since_200', 'latest_exception', 'resource', 'latest_log_entry_date', 'endpoint_entry_date', 'endpoint_end_date', 'resource_start_date', 'resource_end_date', 'rn']
+}
+
+def check_performance_columns():
+    conn = sqlite3.connect("dataset/performance.sqlite3").cursor()
+    result, message, details = check_columns(conn, EXPECTED)
+    if not result:
+        print("Column check failed for performance DB")
+        print(message)
+        for item in details:
+            if not item["success"]:
+                print(f"{item["table"]} did not have all expected columns. Missing columns: {item["missing"]}")
+                print(f"Columns found: {item["actual"]}")
+                print(f"Expected columns: {item["expected"]}")
+    print(result)
+    print(message)
+    print(details)
+
+
+if __name__ == "__main__":
+    check_performance_columns()
