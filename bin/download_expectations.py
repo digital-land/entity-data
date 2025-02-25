7d01fcd8b6b68ecb77d6337565bb8d0ae553c12a
CarlosCoelhoSL <ccoelho@scottlogic.com>
2025-02-25 16:03:36 +0000

    adjust download_expectations

--- bin/download_expectations.py
+++ bin/download_expectations.py
@@ -42,7 +42,7 @@ Path('expectation').mkdir(parents=True, exist_ok=True)
 conn.execute(
     f"""
         COPY (
-            SELECT * FROM '{cache_exp_dir}/*.parquet'
+            SELECT * FROM parquet_scan('{cache_exp_dir}/*.parquet',union_by_name=True)
         ) 
         TO 'expectation/expectation.csv' (FORMAT CSV, HEADER TRUE)
     """
